fb33f39e830dd236ab69a47d768ddda4db940a23
adelsongpjr <Admin605806>
2019-09-06 17:54:45 -0300

    Curso Angular
    Inicial

---
+++ meat-app-starter-master/src/app/restaurants/restaurants.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from './restaurant/restaurant.model';
+
+@Component({
+  selector: 'mt-restaurants',
+  templateUrl: './restaurants.component.html'
+})
+export class RestaurantsComponent implements OnInit {
+
+  restaurants: Restaurant[] 
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
